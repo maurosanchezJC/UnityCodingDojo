7f682a44a90d90700c03b9607ff1750258a7c685
MauroSanchezJC <mauro.sanchez@jamcity.com>
2022-04-05 10:03:28 -0300

    [Dojo #1] Added LSP Effect

---
+++ KataOne/LSPExample/LSPNonSolid.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace KataOne.LSPExample
+{
+    public class NonSolidCardPlayer
+    {
+        public List<Card> Cards { get; set; }
+
+        public void ExecuteCardEffects()
+        {
+            foreach (Card card in Cards)
+            {
+                if (card is EffectCard effect)
+                {
+                    effect.ActivateEffect();
+                }
+            }
+        }
+
+        public void ShowCardNames()
+        {
+            foreach (Card card in Cards)
+            {
+                Console.WriteLine(card.CardType);
+            }
+        }
+    }
+
+    public abstract class Card
+    {
+        public abstract string CardType { get; }
+    }
+
+    public class EffectCard : Card
+    {
+        public override string CardType => "Effect";
+
+        public void ActivateEffect() => Console.WriteLine("Effect triggered!");
+    }
+    
+}
